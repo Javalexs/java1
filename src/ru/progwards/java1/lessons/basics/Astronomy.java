3752ebb14423dbb12f2748296794529247cda6b1
Student1 <fadeevlesha@list.ru>
2020-06-21 23:31:44 +0300

    ASTRONOMY

--- src/ru/progwards/java1/lessons/basics/Astronomy.java
+++ src/ru/progwards/java1/lessons/basics/Astronomy.java
@@ -1,35 +1,31 @@
 package ru.progwards.java1.lessons.basics;
 
-import com.sun.source.util.SourcePositions;
-
-import javax.swing.plaf.synth.SynthOptionPaneUI;
-
 public class Astronomy {
     public static void main(String[] args) {
         System.out.println(sphereSquare (1.0));
-        System.out.println(earthSquare (6371.2));
-        System.out.println(mercurySquare(2439.7));
-        System.out.println(jupiterSquare (71492.0));
-        System.out.println(earthVsMercury( 6371.2, 2439.7));
-        System.out.println(earthVsJupiter (6371.2, 71492.0));
+        System.out.println(earthSquare (6371.2d));
+        System.out.println(mercurySquare(2439.7d));
+        System.out.println(jupiterSquare (71492.0d));
+        System.out.println(earthVsMercury( 6371.2, 2439.7d));
+        System.out.println(earthVsJupiter (6371.2d, 71492.0d));
     }
     public static Double sphereSquare (Double r){
-        double p = 3.14;
+        double p = 3.14d;
         double S = 4*p*r*r;
         return S;
     }
     public static Double earthSquare(Double r1){
-        double p = 3.14;
+        double p = 3.14d;
         double S1 = 4*p*r1*r1;
         return S1;
     }
     public static Double mercurySquare(Double r2){
-        double p = 3.14;
+        double p = 3.14d;
         double S2 = 4*p*r2*r2;
         return S2;
     }
     public static Double jupiterSquare (Double r3){
-        double p = 3.14;
+        double p = 3.14d;
         double S3 = 4*p*r3*r3;
         return S3;
     }
