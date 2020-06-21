0b492cfc4e8dec933e3c54829fe02406e81bbce6
Student1 <fadeevlesha@list.ru>
2020-06-22 00:02:00 +0300

    ASTRONOMY

--- src/ru/progwards/java1/lessons/basics/Astronomy.java
+++ src/ru/progwards/java1/lessons/basics/Astronomy.java
@@ -14,27 +14,27 @@ public class Astronomy {
         double S = 4.0*p*r*r;
         return S;
     }
-    public static Double earthSquare(Double r1){
+    public static Double earthSquare(double r1){
         double p = 3.14d;
         double S1 = 4.0*p*r1*r1;
         return S1;
     }
-    public static Double mercurySquare(Double r2){
+    public static Double mercurySquare(double r2){
         double p = 3.14d;
         double S2 = 4.0*p*r2*r2;
         return S2;
     }
-    public static Double jupiterSquare (Double r3){
+    public static Double jupiterSquare (double r3){
         double p = 3.14d;
         double S3 = 4.0*p*r3*r3;
         return S3;
     }
-    public static Double earthVsMercury (Double r1, Double r2){
-        Double z1 = (r1 * r1) / (r2 * r2);
+    public static Double earthVsMercury (double r1, double r2){
+        double z1 = (r1 * r1) / (r2 * r2);
         return z1;
     }
-    public static Double earthVsJupiter (Double r1, Double r3){
-        Double z2 = (r1 * r1) / (r3 * r3);
+    public static Double earthVsJupiter (double r1, double r3){
+        double z2 = (r1 * r1) / (r3 * r3);
         return z2;
     }
 }
