99ce1caf41ca0b0c1fe106d675a297238bc6b458
Student1 <fadeevlesha@list.ru>
2020-07-05 16:08:46 +0300

    Triangle

---
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -0,0 +1,23 @@
+package ru.progwards.java1.lessons.compare_if_cycles;
+
+public class TriangleSimpleInfo {
+    public static int main(String[] args) {
+        System.out.println (3, 4, 5);
+
+        public static int maxSide(int a, int b, int c){
+
+            if (a > b > c) {
+                return a;
+            }
+            else if (a < b < c){
+                return c;
+            }
+            else{
+                return b;
+            }
+
+
+
+        }
+    }
+}
