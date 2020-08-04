3c1ea71257424b1b34ebf81cd247a5c452affc0d
Student1 <fadeevlesha@list.ru>
2020-08-04 12:27:03 +0300

    fibo done

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -1,36 +1,27 @@
 package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class CyclesGoldenFibo {
+//    public static boolean containsDigit(int number, int digit) {
+//        for
+//    }
+
     public static void main(String[] args) {
-        System.out.println(fiboNumber(1));
-        System.out.println(fiboNumber(2));
-        System.out.println(fiboNumber(3));
-        System.out.println(fiboNumber(4));
-        System.out.println(fiboNumber(5));
-        System.out.println(fiboNumber(6));
-        System.out.println(fiboNumber(7));
-        System.out.println(fiboNumber(8));
-        System.out.println(fiboNumber(9));
-        System.out.println(fiboNumber(10));
-        System.out.println(fiboNumber(11));
-        System.out.println(fiboNumber(12));
-        System.out.println(fiboNumber(13));
-        System.out.println(fiboNumber(14));
-        System.out.println(fiboNumber(15));
+        for(int n = 1; n < 16; n++){
+            System.out.println(fiboNumber(n));
 
+        }
         for (int i = 1; i < 100; i++) {
-            for (int j = 1; j <= i; j++)
-                if (isGoldenTriangle (i,i,j)){
-                    System.out.println("Найден золотой треугольник с параметрами: " + i + ", " + i + ", " + j);
+            for (int j = 1; j <= i; j++){
+                if (isGoldenTriangle(i,i,j)) {
+                    System.out.println ("Найден золотой треугольник с параметрами: " + i + ", " + i + ", " + j);
 
+                }
             }
         }
     }
 
-    public static boolean containsDigit(int number, int digit){
-
-    }
-
+//
+//
     public static int fiboNumber (int n) {
         int x = 0;
         int x1 = 1;
@@ -45,7 +36,8 @@ public class CyclesGoldenFibo {
     }
 
     public static boolean isGoldenTriangle(int a, int b, int c){
-       if (a == b || b == c || a == c && a/c >= 1.61703 && a/c <= 1.61903 || b/c >= 1.61703 && b/c <= 1.61903){
+//       if (a == b || b == c || a == c && a/c >= 1.61703 && a/c <= 1.61903 || b/c >= 1.61703 && b/c <= 1.61903){
+        if(a == b & (double)a/c >= 1.61703d & (double)a/c <= 1.61903d | a == c & (double)a/b  >= 1.61703d & (double)a/b  <= 1.61903d | b == c & (double)b/a  >= 1.61703d & (double)b/a  <= 1.61903d){
             return true;
         } else {
 
