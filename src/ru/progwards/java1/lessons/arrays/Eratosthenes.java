adce56cb91412ecc63814ba7c3588af850377c23
Student1 <fadeevlesha@list.ru>
2020-09-08 23:05:33 +0300

    Eratosthenes 8

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -4,36 +4,36 @@ package ru.progwards.java1.lessons.arrays;
 import java.util.Arrays;
 
 public class Eratosthenes {
+
     int N;
-    private boolean[] sieve = {true, false};
+    private boolean[] sieve;
 
     public Eratosthenes (int N){
         this.N = N;
         sieve = new boolean[N];
         Arrays.fill(sieve,true);
         sift();
-
     }
 
     private void sift() {
-        sieve[1] = true;
+        sieve[1] = false;
         for (int i = 2; i * i < N; i++)
             if (sieve[i])
-                for (int j = 2; j < sieve.length; j++) {
+                for (int j = i * i; j < N; j++)
                     sieve[j] = false;
-                }
+
     }
 
     public boolean isSimple(int n){
         if (n == 0){
             return false;
         }
-        else {
-            return true;
-        }
+        return true;
+
     }
 
     public static void main(String[] args) {
+//        System.out.println(Arrays.toString(sieve));
 
 
     }
