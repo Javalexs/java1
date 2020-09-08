c047b453d7f535c9484ca4477c495125784c1614
Student1 <fadeevlesha@list.ru>
2020-09-08 16:32:45 +0300

    Eratosthenes 4

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -16,7 +16,7 @@ import java.util.Arrays;
     }
 
     private void sift() {
-        sieve[1] = false;
+        sieve[1] = true;
         for (int i = 2; i * i < N; i++)
             if (sieve[i])
                 for (int j = 2; j < sieve.length; j++) {
