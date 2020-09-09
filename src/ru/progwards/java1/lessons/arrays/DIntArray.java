f4ecf83aa47f700d9f7e8ab83b52c1faab6ce6b2
Student1 <fadeevlesha@list.ru>
2020-09-09 23:06:17 +0300

    DIntArray 1

---
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -0,0 +1,30 @@
+package ru.progwards.java1.lessons.arrays;
+
+import java.util.Arrays;
+
+public class DIntArray {
+    private int[] a;
+        public DIntArray(){
+
+            public void add(int num){
+                a = Arrays.copyOf(a, a.length + 1);
+                a[a.length - 1] = num;
+            }
+            public void atInsert(int pos, int num){
+                a = Arrays.copyOf(a, a.length + 1);
+//                a[a.length] = num;
+                a[pos] = num;
+
+            }
+            public void atDelete(int pos){
+                a = Arrays.copyOf(a, a.length - 1);
+
+            }
+            public int at(int pos){
+                return num;
+
+            }
+
+        }
+
+}
