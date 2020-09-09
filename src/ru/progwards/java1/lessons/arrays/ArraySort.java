78b5bb19d158bc053a6fddc513a15fbd4f299c99
Student1 <fadeevlesha@list.ru>
2020-09-09 18:12:43 +0300

    Arrays.sort 3

--- src/ru/progwards/java1/lessons/arrays/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -1,20 +1,18 @@
 package ru.progwards.java1.lessons.arrays;
 
 import java.util.Arrays;
+
 public class ArraySort {
-
-
     public static void main(String[] args) {
 
     }
     public static void sort(int[] a){
         for (int i = 1; i < a.length; i++){
-            if (a[i] > a[i + 1])
-                for (int j = i + 1; j < a.length; j += i) {
+            for (int j = i + 1; j < a.length; j += i) {
                   if (a[i] > a[j]) {
                       Arrays.sort(a);
                   }
-                }
+            }
 
         }
     }
