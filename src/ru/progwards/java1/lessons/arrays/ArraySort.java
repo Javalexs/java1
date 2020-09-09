f821455fa4941008fe7bacfdbb78433176c86e79
Student1 <fadeevlesha@list.ru>
2020-09-09 17:59:12 +0300

    Arrays.sort 2

--- src/ru/progwards/java1/lessons/arrays/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -10,8 +10,9 @@ public class ArraySort {
     public static void sort(int[] a){
         for (int i = 1; i < a.length; i++){
             if (a[i] > a[i + 1]) {
-                for (int j = i + 1; j < a.length; j +=1){
-                    Arrays.sort (a);
+                for (int j = i + 1; j < a.length; j +=i){
+                    if (a[i] > a[j]) {
+                     Arrays.sort (a);
                 }
 
             }
