8fdf26fb90519a73aa10788e7ca473bb3d37f8bd
Student1 <fadeevlesha@list.ru>
2020-09-10 16:05:48 +0300

    DIntArray 6

--- src/ru/progwards/java1/lessons/arrays/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -9,10 +9,10 @@ public class ArraySort {
     }
     public static void sort(int[] a){
         int b;
-        for (int i = 1; i < a.length; i++){
-            for (int j = i + 1; j < a.length; j += i) {
+        for (int i = 0; i < a.length; i++){
+            for (int j = i + 1; j < a.length; j++) {
                   if (a[i] > a[j]) {
-                      int b = a [i];
+                      b = a [i];
                       a[i] = a[j];
                       a[j] = b;
 
