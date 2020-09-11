52cdfc6e74eafacc5357a8e98a3412c8734f4fd7
Student1 <fadeevlesha@list.ru>
2020-09-11 09:29:04 +0300

    DIntArray 11

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -12,11 +12,16 @@ import java.util.Arrays;
             public void add(int num){
                 a = new int[1];
                 int[] b = new int[a.length + 1];
-                System.arraycopy(a, 0, b,0, a.length);
+                System.arraycopy(a, 0, b,0, a.length + 1);
                 b[a.length - 1] = num;
 
             }
             public void atInsert(int pos, int num){
+                a = new int[1];
+                int[] b = new int[a.length + 1];
+                System.arraycopy(a, num + 1, b,num, a.length + 1);
+                b[a.length - 1] = num;
+
 
             }
             public void atDelete(int pos){
