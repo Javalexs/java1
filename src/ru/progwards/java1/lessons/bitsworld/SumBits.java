2aac4d1096357f033b06dd95770e1708d272c735
Student1 <fadeevlesha@list.ru>
2020-09-23 20:12:14 +0300

    sumBits1

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -2,12 +2,16 @@ package ru.progwards.java1.lessons.bitsworld;
 
 public class SumBits {
     public static void main(String[] args) {
-//        System.out.println(sumBits(0101010));
+        System.out.println(sumBits(0b0011));
     }
     public static int sumBits(byte value){
-        int sm = value;
-        while (value > 0) sm += value >>>= 1 & 1;
-        return sm;
+        int sum = 0;
+        for (int i = 0; i < 8; i++) {
+            sum += (0x1) & (value >> i);
+        }
+        return sum;
+
+
 
     }
 }
