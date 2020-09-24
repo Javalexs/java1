99f971a047608a162b4000f6b92bcdd2dce11d96
Student1 <fadeevlesha@list.ru>
2020-09-24 19:50:57 +0300

    sumBits2

---
+++ src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
@@ -0,0 +1,12 @@
+package ru.progwards.java1.lessons.bitsworld;
+
+public class CheckBit {
+
+
+
+
+    public static int checkBit(byte value, int bitNumber){
+        return (value >> bitNumber) & (0x1);
+    }
+
+}
--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -2,12 +2,12 @@ package ru.progwards.java1.lessons.bitsworld;
 
 public class SumBits {
     public static void main(String[] args) {
-//        System.out.println(sumBits(00000011));
+        System.out.println(sumBits(00000011));
     }
     public static int sumBits(byte value){
         int sum = 0;
         for (int i = 0; i < 8; i++) {
-            sum += (0x1) & (value >> i);
+            sum += (value >> i) & (0x1);
         }
         return sum;
     }
