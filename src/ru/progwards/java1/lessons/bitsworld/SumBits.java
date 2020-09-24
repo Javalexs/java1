66589981114d580609edaf0208ecc83b5c32621e
Student1 <fadeevlesha@list.ru>
2020-09-24 20:20:42 +0300

    checkBit1

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -2,7 +2,7 @@ package ru.progwards.java1.lessons.bitsworld;
 
 public class SumBits {
     public static void main(String[] args) {
-        System.out.println(sumBits(00000011));
+
     }
     public static int sumBits(byte value){
         int sum = 0;
