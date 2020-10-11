e61ec5978d7c7d5ef84908799c79e6fa75728bf7
Student1 <fadeevlesha@list.ru>
2020-10-11 15:27:18 +0300

    fibo

--- src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -2,7 +2,7 @@ package ru.progwards.java1.lessons.interfaces;
 
 public class CalculateFibonacci {
 
-    privat static CacheInfo lastFibo;
+    private static CacheInfo lastFibo;
 
     public static class CacheInfo{
         public int n;
