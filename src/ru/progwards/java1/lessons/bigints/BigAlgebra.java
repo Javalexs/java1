de8a79dc1116562834da78c4c1d81f2c901c1a51
Student1 <fadeevlesha@list.ru>
2020-10-19 18:46:39 +0300

    pow

--- src/ru/progwards/java1/lessons/bigints/BigAlgebra.java
+++ src/ru/progwards/java1/lessons/bigints/BigAlgebra.java
@@ -7,9 +7,9 @@ public class BigAlgebra {
     public static BigDecimal fastPow(BigDecimal num, int pow){
         return num.pow(pow);
     }
-    public static BigInteger fibonacci(int n){
-//        return ;
-    }
+//    public static BigInteger fibonacci(int n){
+////        return ;
+//    }
 
     public static void main(String[] args) {
 
