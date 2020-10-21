90e905219a863158fb0837f35c3f5462f69c1c36
Student1 <fadeevlesha@list.ru>
2020-10-21 20:54:02 +0300

    fibo4

--- src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -1,19 +1,13 @@
 package ru.progwards.java1.lessons.interfaces;
-
 public class CalculateFibonacci {
-
     private static CacheInfo lastFibo;
-
     public static class CacheInfo {
         public int n;
         public int fibo;
-
         public CacheInfo(int n, int fibo) {
-            this.n = n;
-            this.fibo = fibo;
+            this.n = n; this.fibo = fibo;
         }
     }
-
     public static int fiboNumber(int n){
         if (lastFibo != null && lastFibo.n == n)
             return lastFibo.fibo;
@@ -24,21 +18,15 @@ public class CalculateFibonacci {
             fibo = fibo1 + fibo2;
             fibo1 = fibo2;
             fibo2 = fibo;
-
         }
         lastFibo = new CacheInfo(n, fibo);
         return fibo;
     }
-
     public static CacheInfo getLastFibo(){
         return lastFibo;
     }
     public static void clearLastFibo() {
         lastFibo = null;
     }
-    public static void main(String[] args) {
-        System.out.println(fiboNumber(3));
-        fiboNumber(1);
-
-    }
+    public static void main(String[] args) { System.out.println(fiboNumber(3));fiboNumber(1); }
 }
