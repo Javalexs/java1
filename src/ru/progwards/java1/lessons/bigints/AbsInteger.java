43477fa272c90c4950d884ede99fc055f4cf7cf8
Student1 <fadeevlesha@list.ru>
2020-10-21 18:55:25 +0300

    Abs

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -1,11 +1,11 @@
 package ru.progwards.java1.lessons.bigints;
 
 abstract public class AbsInteger {
-    abstract int num;
+    int num;
 
-    @Override
+
     public String toString(){
-        return num;
+
     }
 
     public static AbsInteger add(AbsInteger num1, AbsInteger num2){
