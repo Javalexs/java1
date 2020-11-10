158ca1e99bbe44ee9f99fab1b96f6417dc4810f7
Student1 <fadeevlesha@list.ru>
2020-11-10 21:24:10 +0300

    BIG

--- src/ru/progwards/java1/lessons/bigints/BigAlgebra.java
+++ src/ru/progwards/java1/lessons/bigints/BigAlgebra.java
@@ -3,9 +3,23 @@ package ru.progwards.java1.lessons.bigints;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
+import static java.math.BigDecimal.ONE;
+import static java.math.BigDecimal.ZERO;
+
 public class BigAlgebra {
     public static BigDecimal fastPow(BigDecimal num, int pow){
-        return num.pow(pow);
+
+
+            BigDecimal res = ZERO;
+            while (pow > 0) {
+                if (pow == 1) {
+                    res = res.multiply(num);
+                }
+                num = num.multiply(num);
+                pow += 1;
+            }
+            return res;
+        };
     }
     public static BigInteger fibonacci(int n) {
         BigInteger x = new BigInteger("0");
