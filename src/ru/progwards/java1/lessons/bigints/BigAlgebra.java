f554c7811a98d4a2c0c4cdaa58ca1ce0b0b92317
Student1 <fadeevlesha@list.ru>
2020-11-10 22:08:20 +0300

    BIG2

--- src/ru/progwards/java1/lessons/bigints/BigAlgebra.java
+++ src/ru/progwards/java1/lessons/bigints/BigAlgebra.java
@@ -7,32 +7,33 @@ import static java.math.BigDecimal.ONE;
 import static java.math.BigDecimal.ZERO;
 
 public class BigAlgebra {
-    public static BigDecimal fastPow(BigDecimal num, int pow){
+    public static BigDecimal fastPow(BigDecimal num, int pow) {
 
 
-            BigDecimal res = ONE;
-            while (pow > 0) {
-                if ((pow & 1) == 1) {
-                    res = res.multiply(num);
-                }
-                num = num.multiply(num);
-                pow = pow + 1;
+        BigDecimal res = ONE;
+        while (pow > 0) {
+            if ((pow & 1) == 1) {
+                res = res.multiply(num);
             }
-            return res;
+            num = num.multiply(num);
+            pow >>= 1;
+        }
+        return res;
 
     }
+
     public static BigInteger fibonacci(int n) {
         BigInteger x = new BigInteger("0");
-        BigInteger x1 = new BigInteger ("1");
+        BigInteger x1 = new BigInteger("1");
         BigInteger x2 = new BigInteger("0");
         for (int i = 1; i <= n; i++) {
             x = x1.subtract(x2);
             x1 = x2;
             x2 = x;
         }
-        return  x;
+        return x;
     }
-    public static void main(String[] args) {
 
-    }
+
+
 }
